362c3d33bd21fb72e8e1652b41203ca01a42ea03
Gsdvl <gsdvl2004@gmail.com>
2023-11-19 02:10:38 -0300

    Commit teste com correções iniciais

--- ProjetoFinal-LPII/src/sistema/Login.java
+++ ProjetoFinal-LPII/src/sistema/Login.java
@@ -2,8 +2,8 @@ package src.sistema;
 
 import javax.swing.*;
 
-public class Tela {
-    public Tela(){
+public class Login {
+    public Login(){
         JFrame frame = new JFrame();
         frame.setVisible(true);
         frame.setSize(800,500);
